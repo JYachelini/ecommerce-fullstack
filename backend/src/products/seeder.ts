5a4aa2760c742879a785ff74117ae55ded092203
julian <juliyachedev@gmail.com>
2022-08-21 00:40:27 -0300

    fix url secret

--- backend/src/products/seeder.ts
+++ backend/src/products/seeder.ts
@@ -1,13 +1,19 @@
+import { ConfigModule, ConfigType } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { seeder } from 'nestjs-seeder';
+import config from '../config/config';
 import { ProductSeeder } from './products.seeder';
 import { Product, ProductSchema } from './schemas/products.schema';
 
 seeder({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://julian:yYh1YRWZiJ0EcbHi@cluster0.ovaqa.mongodb.net/ecommerce?retryWrites=true',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => ({
+        uri: configService.MONGODB_URI,
+      }),
+    }),
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
   ],
 }).run([ProductSeeder]);
